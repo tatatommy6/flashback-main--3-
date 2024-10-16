7e967a79646a40438cef2096ae22d9cbd4a281aa
김민결 <139607260+tatatommy6@users.noreply.github.com>
2024-10-17 00:30:51 +0900

    yey

--- runway.py
+++ runway.py
@@ -2,6 +2,7 @@ import os
 import time
 import dotenv
 from runwayml import RunwayML
+import requests
 
 # .env 파일에서 환경 변수 불러오기
 dotenv.load_dotenv()
@@ -10,15 +11,19 @@ dotenv.load_dotenv()
 client = RunwayML(api_key=os.getenv('RUNWAY_API_SECRET'))
 
 # 이미지에서 비디오 생성 작업
-image_to_video = client.image_to_video.create(
-    model="gen3a_turbo",
-    prompt_image="https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-    prompt_text="a cute cat",
+task = client.image_to_video.create(
+  model='gen3a_turbo',
+  # Point this at your own image file
+  prompt_image='https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg',
+  prompt_text='string',
 )
+task_id = task.id
 
-# 생성된 영상의 ID
-job_id = image_to_video.id
-print(f"{job_id}")
+# Poll the task until it's complete
+time.sleep(10)  # Wait for a second before polling
+task = client.tasks.retrieve(task_id)
+while task.status not in ['SUCCEEDED', 'FAILED']:
+  time.sleep(10)  # Wait for ten seconds before polling
+  task = client.tasks.retrieve(task_id)
 
-# 작업 상태 확인 및 백오프 전략 적용
-
+print('Task complete:', task)
